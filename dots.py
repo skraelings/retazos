92a16f84e315addf37e2581ad755d1c18aca529e
Reynaldo Baquerizo <reynaldomic@gmail.com>
2011-02-08 12:59:04 -0500

    Site subclassing QGraphicsItem

--- dots.py
+++ dots.py
@@ -12,80 +12,95 @@ from PyQt4.QtCore import *
 
 SCENESIZE = (320, 240)
 
-class Site(QGraphicsEllipseItem):
+class Site(QGraphicsItem):
 
     def __init__(self, rect, weight):
-        super(Site, self).__init__(rect)
+        super(Site, self).__init__()
         self.setFlags(QGraphicsItem.ItemIsSelectable |
                       QGraphicsItem.ItemIsMovable |
                       QGraphicsItem.ItemIsFocusable)
         self.rect = rect
         self.weight = weight            # Controls how much the Site is allowed
-                                        # to move
+                                        # to move. Range is 0-1. If 0, no movemente.
         self.setFocus()
+
+    def boundingRect(self):
+        return self.rect
+
+    # call by scene.update !!
+    def paint(self, painter, option, widget=None):
+        painter.setBrush(QBrush(Qt.blue))
+        painter.drawRect(self.rect)
 
 class SiteDialog(QDialog):
 
     def __init__(self, parent=None):
         super(SiteDialog, self).__init__(parent)
 
-        self.weightSpinBox = QDoubleSpinBox()
-        self.weightSpinBox.setRange(0, 1)
-        self.weightSpinBox.setSingleStep(0.01)
+        self.weight_label = QLabel("Weight: ")
+        self.weight_spinbox = QDoubleSpinBox()
+        self.weight_spinbox.setRange(0, 1)
+        self.weight_spinbox.setSingleStep(0.01)
 
-        okButton = QPushButton("&Ok")
-        cancelButton = QPushButton("Cancel")
+        ok_button = QPushButton("&Ok")
+        cancel_button = QPushButton("Cancel")
 
-        buttonLayout = QHBoxLayout()
-        buttonLayout.addStretch()
-        buttonLayout.addWidget(okButton)
-        buttonLayout.addWidget(cancelButton)
+        properties_layout = QHBoxLayout()
+        # properties_layout.addStretch()
+        properties_layout.addWidget(self.weight_label)
+        properties_layout.addWidget(self.weight_spinbox)
+
+        button_layout = QHBoxLayout()
+        button_layout.addStretch()
+        button_layout.addWidget(ok_button)
+        button_layout.addWidget(cancel_button)
 
         layout = QVBoxLayout()
-        layout.addWidget(self.weightSpinBox)
-        layout.addLayout(buttonLayout)
+        layout.addLayout(properties_layout)
+        layout.addLayout(button_layout)
         self.setLayout(layout)
 
-        self.connect(okButton, SIGNAL("clicked()"), self, SLOT("accept()"))
-        self.connect(cancelButton, SIGNAL("clicked()"), self, SLOT("reject()"))
+        self.connect(ok_button, SIGNAL("clicked()"), self, SLOT("accept()"))
+        self.connect(cancel_button, SIGNAL("clicked()"), self, SLOT("reject()"))
         self.setWindowTitle("Site Properties")
 
 class MainForm(QDialog):
 
     def __init__(self, parent=None):
         super(MainForm, self).__init__(parent)
-        self.scene = QGraphicsScene(self)
-        self.scene.setSceneRect(0, 0, *SCENESIZE)
         self.view = QGraphicsView()
         self.view.setRenderHint(QPainter.Antialiasing)
+        self.scene = QGraphicsScene(self)
+        self.scene.setSceneRect(0, 0, *SCENESIZE)
         self.view.setScene(self.scene)
 
         self.siteLabel = QLabel("Site: ")
 
-        buttonLayout = QVBoxLayout()
+        button_layout = QVBoxLayout()
         for text, slot in (("Add &Site", self.add_site),):
             button = QPushButton(text)
             self.connect(button, SIGNAL("clicked()"), slot)
-            buttonLayout.addWidget(button)
-        buttonLayout.addStretch()
+            button_layout.addWidget(button)
+        button_layout.addStretch()
 
-        displayLayout = QVBoxLayout()
-        displayLayout.addWidget(self.view)
-        displayLayout.addWidget(self.siteLabel)
-        displayLayout.addStretch()
+        display_layout = QVBoxLayout()
+        display_layout.addWidget(self.view)
+        display_layout.addWidget(self.siteLabel)
+        display_layout.addStretch()
 
         layout = QHBoxLayout()
-        layout.addLayout(displayLayout)
-        layout.addLayout(buttonLayout)
+        layout.addLayout(display_layout)
+        layout.addLayout(button_layout)
         self.setLayout(layout)
 
     def add_site(self):
         form = SiteDialog()
         if form.exec_():
-            weight = form.weightSpinBox.value()
+            weight = form.weight_spinbox.value()
             self.scene.addItem(Site(QRectF(random() * SCENESIZE[0],
                                            random() * SCENESIZE[1],
                                            8, 8), weight))
+            self.scene.update()
 
 def main():
     app = QApplication(sys.argv)
